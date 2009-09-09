19885bec837cf3e5e1c30b29bf8d55b155194cb0
xdanielDZD <xdanielDZD@a09211ee-a223-11dd-9dc9-65bc890fb60a>
2009-09-09 20:37:01 +0000

    OZMAV: Changed debug.h to only contain the variable declarations, not the actual array data, as per zzt32's comment to the last revision. For some reason, this actually fixes the Wavefront obj dumping which I must have unknowingly broken before (OZMAV crashed when trying to load a level while it was enabled).

---
+++ debug.c
@@ -0,0 +1,36 @@
+const char * CombinerTypes32[32] = {
+	"COMBINED        ", "TEXEL0          ",
+	"TEXEL1          ", "PRIMITIVE       ",
+	"SHADE           ", "ENVIRONMENT     ",
+	"1               ", "COMBINED_ALPHA  ",
+	"TEXEL0_ALPHA    ", "TEXEL1_ALPHA    ",
+	"PRIMITIVE_ALPHA ", "SHADE_ALPHA     ",
+	"ENV_ALPHA       ", "LOCFRAC         ",
+	"PRIMLOCFRAC     ", "K5              ",
+	"<unknown>       ", "<unknown>       ",
+	"<unknown>       ", "<unknown>       ",
+	"<unknown>       ", "<unknown>       ",
+	"<unknown>       ", "<unknown>       ",
+	"<unknown>       ", "<unknown>       ",
+	"<unknown>       ", "<unknown>       ",
+	"<unknown>       ", "<unknown>       ",
+	"<unknown>       ", "0               "
+};
+
+const char * CombinerTypes16[16] = {
+	"COMBINED        ", "TEXEL0          ",
+	"TEXEL1          ", "PRIMITIVE       ",
+	"SHADE           ", "ENVIRONMENT     ",
+	"1               ", "COMBINED_ALPHA  ",
+	"TEXEL0_ALPHA    ", "TEXEL1_ALPHA    ",
+	"PRIMITIVE_ALPHA ", "SHADE_ALPHA     ",
+	"ENV_ALPHA       ", "LOCFRAC         ",
+	"PRIMLOCFRAC     ", "0               "
+};
+
+const char * CombinerTypes8[8] = {
+	"COMBINED        ", "TEXEL0          ",
+	"TEXEL1          ", "PRIMITIVE       ",
+	"SHADE           ", "ENVIRONMENT     ",
+	"1               ", "0               "
+};
