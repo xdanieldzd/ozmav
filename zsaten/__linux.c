412c9d008718a6fc656f8bb1f8f984736f84398e
spinout_182 <spinout_182@yahoo.com>
2010-11-10 22:51:36 +0000

    Look who was signed.

--- zsaten/__linux.c
+++ zsaten/__linux.c
@@ -72,6 +72,7 @@ int XMain()
 				kevent = (XKeyEvent *) &event;
 				if((XLookupString((XKeyEvent *)&event,buffer,1,&keysym,NULL) == 1) && (keysym == (KeySym)XK_Escape)) vProgram.isRunning = false;
 				vProgram.key[keysym & 0xFF] = true;
+				dbgprintf(0, MSK_COLORTYPE_INFO, "key: %i : %i %i %i %i", (keysym & 0xFF), XK_Up, XK_Down, XK_Left, XK_Right);
 				break; }
 			case KeyRelease: {
 				KeySym     keysym;
@@ -113,23 +114,23 @@ int XMain()
 	if(vProgram.key[XK_f]) ca_Movement(true, -0.5f);
 	if(vProgram.key[XK_h]) ca_Movement(true, 0.5f);
 
-	if(vProgram.key[XK_Up]) vCamera.actorRotX -= 2.0f;
-	if(vProgram.key[XK_Down]) vCamera.actorRotX += 2.0f;
-	if(vProgram.key[XK_Left]) vCamera.actorRotY -= 2.0f;
-	if(vProgram.key[XK_Right]) vCamera.actorRotY += 2.0f;
+	if(vProgram.key[XK_Up&0xFF]) vCamera.actorRotX -= 2.0f;
+	if(vProgram.key[XK_Down&0xFF]) vCamera.actorRotX += 2.0f;
+	if(vProgram.key[XK_Left&0xFF]) vCamera.actorRotY -= 2.0f;
+	if(vProgram.key[XK_Right&0xFF]) vCamera.actorRotY += 2.0f;
 
-	if(vProgram.key[XK_Tab]) {
+	if(vProgram.key[XK_Tab&0xFF]) {
 		vProgram.showBones ^= 1;
-		vProgram.key[XK_Tab] = false;
+		vProgram.key[XK_Tab&0xFF] = false;
 	}
 
-	if(vProgram.key[XK_space]) {
+	if(vProgram.key[XK_space&0xFF]) {
 		vProgram.animPlay ^= 1;
-		vProgram.key[XK_space] = false;
+		vProgram.key[XK_space&0xFF] = false;
 	}
 
-	if(vProgram.key[XK_F1]) {
-		vProgram.key[XK_F1] = false;
+	if(vProgram.key[XK_F1&0xFF]) {
+		vProgram.key[XK_F1&0xFF] = false;
 		if(vCurrentActor.actorNumber > 0) {
 			vCurrentActor.actorNumber--;
 			vCurrentActor.animCurrent = 0;
@@ -138,8 +139,8 @@ int XMain()
 		}
 	}
 
-	if(vProgram.key[XK_F2]) {
-		vProgram.key[XK_F2] = false;
+	if(vProgram.key[XK_F2&0xFF]) {
+		vProgram.key[XK_F2&0xFF] = false;
 		if(vCurrentActor.actorNumber < vZeldaInfo.actorCount - 1) {
 			vCurrentActor.actorNumber++;
 			vCurrentActor.animCurrent = 0;
@@ -148,70 +149,70 @@ int XMain()
 		}
 	}
 
-	if(vProgram.key[XK_F3]) {
+	if(vProgram.key[XK_F3&0xFF]) {
 		if(vCurrentActor.animCurrent > 0) {
 			vCurrentActor.animCurrent--;
 			vCurrentActor.frameCurrent = 0;
 		}
-		vProgram.key[XK_F3] = false;
+		vProgram.key[XK_F3&0xFF] = false;
 	}
 
-	if(vProgram.key[XK_F4]) {
+	if(vProgram.key[XK_F4&0xFF]) {
 		if(vCurrentActor.animCurrent < vCurrentActor.animTotal) {
 			vCurrentActor.animCurrent++;
 			vCurrentActor.frameCurrent = 0;
 		}
-		vProgram.key[XK_F4] = false;
+		vProgram.key[XK_F4&0xFF] = false;
 	}
 
-	if(vProgram.key[XK_F5]) {
+	if(vProgram.key[XK_F5&0xFF]) {
 		if(vCurrentActor.boneSetupCurrent > 0) {
 			vCurrentActor.boneSetupCurrent--;
 			vCurrentActor.frameCurrent = 0;
 		}
-		vProgram.key[XK_F5] = false;
+		vProgram.key[XK_F5&0xFF] = false;
 	}
 
-	if(vProgram.key[XK_F6]) {
+	if(vProgram.key[XK_F6&0xFF]) {
 		if(vCurrentActor.boneSetupCurrent < vCurrentActor.boneSetupTotal) {
 			vCurrentActor.boneSetupCurrent++;
 			vCurrentActor.frameCurrent = 0;
 		}
-		vProgram.key[XK_F6] = false;
+		vProgram.key[XK_F6&0xFF] = false;
 	}
 
-	if(vProgram.key[XK_F9]) {
+	if(vProgram.key[XK_F9&0xFF]) {
 		ca_Reset();
-		vProgram.key[XK_F9] = false;
+		vProgram.key[XK_F9&0xFF] = false;
 	}
 
-	if(vProgram.key[XK_F12]) {
+	if(vProgram.key[XK_F12&0xFF]) {
 		vProgram.enableHUD ^= 1;
-		vProgram.key[XK_F12] = false;
+		vProgram.key[XK_F12&0xFF] = false;
 	}
 
-	if(vProgram.key[XK_KP_Divide]) {
+	if(vProgram.key[XK_KP_Divide&0xFF]) {
 		if(vProgram.targetFPS > 15.0f) vProgram.targetFPS -= 15.0f;
-		vProgram.key[XK_KP_Divide] = false;
+		vProgram.key[XK_KP_Divide&0xFF] = false;
 	}
 
-	if(vProgram.key[XK_KP_Multiply]) {
+	if(vProgram.key[XK_KP_Multiply&0xFF]) {
 		if(vProgram.targetFPS <= 60.0f) vProgram.targetFPS += 15.0f;
-		vProgram.key[XK_KP_Multiply] = false;
+		vProgram.key[XK_KP_Multiply&0xFF] = false;
 	}
 
-	if(vProgram.key[XK_KP_Add]) {
+	if(vProgram.key[XK_KP_Add&0xFF]) {
 		if(vProgram.animPlay) vProgram.animPlay = false;
 		if(vCurrentActor.frameCurrent < vCurrentActor.frameTotal) vCurrentActor.frameCurrent++;
 		else vCurrentActor.frameCurrent = 0;
-		vProgram.key[XK_KP_Add] = false;
+		vProgram.key[XK_KP_Add&0xFF] = false;
 	}
 
-	if(vProgram.key[XK_KP_Subtract]) {
+	if(vProgram.key[XK_KP_Subtract&0xFF]) {
 		if(vProgram.animPlay) vProgram.animPlay = false;
 		if(vCurrentActor.frameCurrent > 0) vCurrentActor.frameCurrent--;
 		else vCurrentActor.frameCurrent = vCurrentActor.frameTotal;
-		vProgram.key[XK_KP_Subtract] = false;
+		vProgram.key[XK_KP_Subtract&0xFF] = false;
 	}
 
 	return EXIT_SUCCESS;
